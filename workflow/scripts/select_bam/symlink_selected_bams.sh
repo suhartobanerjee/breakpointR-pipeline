052cd6a4216135f53c125fb0dbdebfa8b47a502f
Suharto Banerjee <suharto.banerjee@mdc-berlin.de>
2024-02-16 11:41:40 +0100

    symlink bai working
    also, no errors folder

--- workflow/scripts/select_bam/symlink_selected_bams.sh
+++ workflow/scripts/select_bam/symlink_selected_bams.sh
@@ -7,3 +7,7 @@ cp $1 $3/.
 
 awk '$2 == 1 {print $1}' $1 \
     | parallel -j $(nproc) -I file ln -s $2/file $3/file
+
+# for bai
+awk '$2 == 1 {print $1}' $1 \
+    | parallel -j $(nproc) -I file ln -s $2/file.bai $3/file.bai
